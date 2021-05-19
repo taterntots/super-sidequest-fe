32bf86dad9a92062d41ed19a9606289ab10bd24a
taterntots <matthew.a.weidner@gmail.com>
2021-05-18 21:32:48 -0400

    got react-router working. Navbar can swap components being rendered

--- src/App.js
+++ src/App.js
@@ -1,61 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+
+// STYLING
+import './App.css';
+
+// COMPONENTS
 import { Counter } from './features/counter/Counter';
 import { User } from './features/user/User';
 import NavBar from './components/NavBar';
-import './App.css';
+import HomePage from './components/HomePage';
+
+// ROUTING
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// ----------------------------------------------------------------------------------
+// ----------------------------------- APP ------------------------------------------
+// ----------------------------------------------------------------------------------
 
 function App() {
   return (
-    <div className="App">
-        <NavBar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <User />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/counter' component={Counter} />
+        <Route path='/support' component={User} />
+      </Switch>
+    </>
   );
 }
 
