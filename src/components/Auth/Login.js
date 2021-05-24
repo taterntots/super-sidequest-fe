2728c01a60f8a7468932b0a8614a1446423ac343
taterntots <matthew.a.weidner@gmail.com>
2021-05-23 21:52:42 -0400

    added error reporting for login page if inputs are not emails or left blank

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -9,6 +9,9 @@ import { appendErrors, useForm } from "react-hook-form";
 // TOAST
 import cogoToast from 'cogo-toast';
 
+// IMAGES
+import { ReactComponent as LoadingSpinner } from '../../img/LoadingSpinner.svg';
+
 // COMPONENTS
 // import CustomConfirmation from './CustomConfirmation';
 // import CustomResetPassword from './CustomResetPassword';
@@ -33,20 +36,9 @@ const Login = () => {
   // React Router
   const history = useHistory();
 
-  // const handleInputChange = (event) => {
-  //   setInput({ ...input, [event.target.name]: event.target.value.toLowerCase().replace(/ /g, "") });
-  // };
-
   // Function to handle submitting Login form
   const onSubmit = async (data) => {
     setSubmitting(true);
-
-    if (!data.username && !data.password) {
-      setSubmitting(false);
-      cogoToast.error('All fields are empty', {
-        hideAfter: 3,
-      });
-    }
 
     // console.log(data.username.toLowerCase().replace(/ /g, ""))
     console.log(data)
@@ -97,11 +89,10 @@ const Login = () => {
             <input
               name='username'
               type='username'
-              // value={value}
               placeholder='Enter your username'
-              // onChange={handleInputChange}
-              className='form-control w-full flex items-center mb-7 mt-3 p-2 rounded-md text-lg'
+              className='form-control text-black w-full flex items-center mb-7 mt-3 p-2 rounded-md text-lg'
               {...register('username', {
+                required: 'Required field',
                 pattern: {
                   value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
                   message: "(Invalid email address)"
@@ -112,14 +103,17 @@ const Login = () => {
           </div>
           <div className="form-group">
             <label className='mr-3'>Password</label>
+            {errors.password && (
+              <span className='text-red-500'>{errors.password.message}</span>
+            )}
             <input
               name='password'
               type='password'
-              // value={input.password}
               placeholder='Enter your password'
-              className='form-control w-full flex items-center mb-3 mt-3 p-2 rounded-md text-lg'
-              {...register('password')}
-            />
+              className='form-control text-black w-full flex items-center mb-3 mt-3 p-2 rounded-md text-lg'
+              {...register('password', {
+                required: 'Required field'
+              })} />
           </div>
           <div className='mb-9'>
             <p>Forgot your password?
@@ -149,16 +143,8 @@ const Login = () => {
               className={`${submitting && 'opacity-50 pointer-events-none'
                 } flex items-center rounded-lg text-lg px-12 py-3 text-center font-medium bg-purplebutton hover:bg-white hover:text-purplebutton focus:ring transition duration-150 ease-in-out`}>
               {submitting && (
-                <svg
-                  stroke="currentColor"
-                  fill="currentColor"
-                  strokeWidth="0"
-                  viewBox="0 0 1024 1024"
-                  className="w-5 h-5 mr-3 animate-spin"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path d="M988 548c-19.9 0-36-16.1-36-36 0-59.4-11.6-117-34.6-171.3a440.45 440.45 0 0 0-94.3-139.9 437.71 437.71 0 0 0-139.9-94.3C629 83.6 571.4 72 512 72c-19.9 0-36-16.1-36-36s16.1-36 36-36c69.1 0 136.2 13.5 199.3 40.3C772.3 66 827 103 874 150c47 47 83.9 101.8 109.7 162.7 26.7 63.1 40.2 130.2 40.2 199.3.1 19.9-16 36-35.9 36z"></path>
-                </svg>
-              )}{' '}
+                <LoadingSpinner />
+              )}
               SIGN IN
               </button>
           </div>
