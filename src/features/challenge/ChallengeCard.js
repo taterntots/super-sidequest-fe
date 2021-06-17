bed4b2928637feb97eb47841062e4095c87e5059
taterntots <matthew.a.weidner@gmail.com>
2021-06-17 17:53:24 -0400

    styled the challengeCard to be more mobile responsive

--- src/features/challenge/ChallengeCard.js
+++ src/features/challenge/ChallengeCard.js
@@ -12,34 +12,35 @@ const ChallengeCard = ({ data }) => {
     username,
     game_title,
     banner_pic_URL,
-    system
+    system,
+    difficulty
   } = data;
 
   return (
     <div className='p-2 rounded-lg hover:bg-purple-500 transform transition duration-500 hover:scale-105'>
       <div
         key={challenge_id}
-        className='flex'
+        className='md:flex'
       >
         <img
-          className='h-24 w-44 rounded-md'
+          className='md:h-24 md:w-44 rounded-md'
           src={banner_pic_URL}
           alt='banner for a single challenge'
         />
-        <div className='flex justify-between w-full'>
-          <div className='ml-4 self-center'>
+        <div className='md:flex md:justify-between text-center md:text-left w-full'>
+          <div className='md:ml-4 self-center mt-4 md:mt-0'>
             <p>{name}</p>
             <p>{game_title}</p>
             <p>by {username}</p>
-
           </div>
-          <div className='text-full'>
+          <div className='flex justify-evenly md:flex-col text-full mt-4 md:mt-0'>
             <p className='truncate px-2 border-2 rounded-md'>{system}</p>
+            <p className='truncate px-2 border-2 rounded-md'>{difficulty}</p>
           </div>
         </div>
       </div>
       <div className='text-center'>
-        <p className='mt-2 border-2 px-10 rounded-md bg-profiletwo'>
+        <p className='mt-4 border-2 px-10 rounded-md bg-profiletwo'>
           {description}
         </p>
       </div>
