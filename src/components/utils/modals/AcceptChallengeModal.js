11fecbe7b53cba63dc508620dadcc85679d5a232
taterntots <matthew.a.weidner@gmail.com>
2021-06-21 17:06:31 -0400

    restyled the abandon and accept challenge models to match the rest of the site

--- src/components/utils/modals/AcceptChallengeModal.js
+++ src/components/utils/modals/AcceptChallengeModal.js
@@ -44,40 +44,31 @@ const AcceptChallengeModal = ({ open, setOpen, submitChallengeAccepted }) => {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-              <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <div className="sm:flex sm:items-start">
-                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                    {/* <ExclamationIcon className="h-6 w-6 text-red-600" aria-hidden="true" /> */}
-                  </div>
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
-                      Accept Quest
-                    </Dialog.Title>
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-500">
-                        Are you sure you want to accept this quest?
-                      </p>
-                    </div>
-                  </div>
+            <div className="inline-block w-full mx-6 align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+              <div className="p-10 bg-taterpurple rounded-lg text-white">
+                <h4 className='text-2xl text-center mb-4'>
+                  Accept Quest
+                </h4>
+                <p className='text-center mb-4'>
+                  Are you sure you want to accept this quest?
+                </p>
+                <div className='flex justify-evenly'>
+                  <button
+                    type="button"
+                    className='flex items-center rounded-lg text-lg px-12 md:px-12 py-3 text-center font-medium bg-purplebutton hover:bg-white hover:text-purplebutton focus:ring transition duration-150 ease-in-out'
+                    onClick={() => setOpen(false)}
+                    ref={cancelButtonRef}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="button"
+                    className='flex items-center rounded-lg text-lg px-12 md:px-12 py-3 text-center font-medium bg-green-400 hover:bg-white hover:text-green-400 focus:ring transition duration-150 ease-in-out'
+                    onClick={submitChallengeAccepted}
+                  >
+                    Accept
+                  </button>
                 </div>
-              </div>
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <button
-                  type="button"
-                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-                  onClick={submitChallengeAccepted}
-                >
-                  Accept
-                </button>
-                <button
-                  type="button"
-                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                  onClick={() => setOpen(false)}
-                  ref={cancelButtonRef}
-                >
-                  Cancel
-                </button>
               </div>
             </div>
           </Transition.Child>
