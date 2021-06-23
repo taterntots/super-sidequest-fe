329224abf5c0791b31bef81e97b847c10bea74b0
taterntots <matthew.a.weidner@gmail.com>
2021-06-23 17:34:24 -0400

    swapped the iframe for ReactPlayer in the video modal. should allow for any video link to work

--- src/components/utils/modals/VideoModal.js
+++ src/components/utils/modals/VideoModal.js
@@ -2,6 +2,9 @@
 import { Fragment, useRef } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 
+// REACT PLAYER
+import ReactPlayer from 'react-player';
+
 // ----------------------------------------------------------------------------------
 // ---------------------------------- VIDEO MODAL -----------------------------------
 // ----------------------------------------------------------------------------------
@@ -44,18 +47,16 @@ const VideoModal = ({ open, setOpen, currentPlayer }) => {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-              <iframe
-                width="515"
-                height="290"
-                src={currentPlayer.video_URL}
-                frameBorder="0"
-                allowFullScreen>
-              </iframe>
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <div className='inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full'>
+              <ReactPlayer
+                url={currentPlayer.video_URL}
+                width='100%'
+                muted={true}
+                controls
+              />
+              <div className='bg-taterpurple px-4 py-4 sm:px-6 sm:flex sm:flex-row-reverse'>
                 <button
-                  type="button"
-                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                  className={`w-full inline-flex justify-center rounded-lg text-lg px-12 py-3 mb-4 md:mb-0 font-medium text-white bg-purplebutton hover:bg-white hover:text-purplebutton focus:ring transition duration-150 ease-in-out`}
                   onClick={() => setOpen(false)}
                   ref={cancelButtonRef}
                 >
