8b89375ca040eeeeb95ebf0dd4f02fd493e670e2
taterntots <matthew.a.weidner@gmail.com>
2021-06-29 18:11:58 -0400

    resylted stats board to match challenge leaderboard

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -34,7 +34,7 @@ const ProfilePage = ({ acceptedChallenges, challenge_game_stats, featured_challe
               <Link
                 key={gameStat.game}
                 to={`/games/${gameStat.game_id}/challenges`}
-                className={`flex justify-between ${index % 2 ? 'bg-gray-600' : 'bg-gray-500'} px-4 py-1 hover:bg-white hover:text-profiletwo`}
+                className={`flex justify-between font-medium ${index % 2 ? 'bg-gray-600' : 'bg-gray-500'} px-4 py-1 hover:opacity-60`}
               >
                 <p>{gameStat.game}</p>
                 <p className='pl-4'>{gameStat.total_challenges_completed}</p>
