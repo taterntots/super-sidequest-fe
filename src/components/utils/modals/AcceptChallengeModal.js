996df952a1258d4324931b74135fb892d12b4717
taterntots <matthew.a.weidner@gmail.com>
2021-07-05 18:52:05 -0400

    accept and abandon modals are mobile responsive

--- src/components/utils/modals/AcceptChallengeModal.js
+++ src/components/utils/modals/AcceptChallengeModal.js
@@ -12,58 +12,58 @@ const AcceptChallengeModal = ({ open, setOpen, submitChallengeAccepted }) => {
   return (
     <Transition.Root show={open} as={Fragment}>
       <Dialog
-        as="div"
+        as='div'
         static
-        className="fixed z-10 inset-0 overflow-y-auto"
+        className='fixed z-10 inset-0 overflow-y-auto'
         initialFocus={cancelButtonRef}
         open={open}
         onClose={setOpen}
       >
-        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className='flex items-center justify-center min-h-screen text-center block'>
           <Transition.Child
             as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+            enter='ease-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
           >
-            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            <Dialog.Overlay className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
           </Transition.Child>
           {/* This element is to trick the browser into centering the modal contents. */}
-          <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+          <span className='hidden sm:inline-block sm:align-middle sm:h-screen' aria-hidden='true'>
             &#8203;
           </span>
           <Transition.Child
             as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enterTo="opacity-100 translate-y-0 sm:scale-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enter='ease-out duration-300'
+            enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+            enterTo='opacity-100 translate-y-0 sm:scale-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100 translate-y-0 sm:scale-100'
+            leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
           >
-            <div className="inline-block w-full mx-6 align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-              <div className="p-10 bg-taterpurple rounded-lg text-white">
+            <div className='inline-block w-full mx-6 align-middle rounded-lg text-left overflow-hidden shadow-xl transform transition-all max-w-xl'>
+              <div className='p-10 bg-taterpurple rounded-lg text-white'>
                 <h4 className='text-2xl text-center mb-4'>
                   Accept Quest
                 </h4>
                 <p className='text-center mb-4'>
                   Are you sure you want to accept this quest?
                 </p>
-                <div className='flex justify-evenly'>
+                <div className='sm:flex sm:justify-evenly'>
                   <button
-                    type="button"
-                    className='flex items-center rounded-lg text-lg px-12 md:px-12 py-3 text-center font-medium bg-purplebutton hover:bg-white hover:text-purplebutton focus:ring transition duration-150 ease-in-out'
+                    type='button'
+                    className='flex w-full sm:w-auto justify-center items-center rounded-lg text-lg sm:px-12 py-3 mb-4 sm:mb-0 text-center font-medium bg-purplebutton hover:bg-white hover:text-purplebutton focus:ring transition duration-150 ease-in-out'
                     onClick={() => setOpen(false)}
                     ref={cancelButtonRef}
                   >
                     Cancel
                   </button>
                   <button
-                    type="button"
-                    className='flex items-center rounded-lg text-lg px-12 md:px-12 py-3 text-center font-medium bg-addgreen hover:bg-white hover:text-addgreen focus:ring transition duration-150 ease-in-out'
+                    type='button'
+                    className='flex w-full sm:w-auto justify-center items-center rounded-lg text-lg sm:px-12 py-3 sm:mb-0 text-center font-medium bg-addgreen hover:bg-white hover:text-addgreen focus:ring transition duration-150 ease-in-out'
                     onClick={submitChallengeAccepted}
                   >
                     Accept
