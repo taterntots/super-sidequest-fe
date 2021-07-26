e6d073c45acf97c3afd3301f206c355b15dea732
taterntots <matthew.a.weidner@gmail.com>
2021-07-26 19:08:44 -0400

    fixed userLeaderboard headers to not bleed out of the lines when mobile

--- src/features/user/UserLeaderboard.js
+++ src/features/user/UserLeaderboard.js
@@ -24,13 +24,9 @@ const UserLeaderboard = ({ users }) => {
         {/* LEADERBOARD HEADERS */}
         <div className='rounded-lg bg-gray-700 my-2'>
           <div className='flex w-full text-center px-2 py-1 font-bold'>
-            <p className='w-1/12'>Rank</p>
-            <p className='w-10/12'>
-              Player
-            </p>
-            <p className='w-1/12'>
-              Level
-            </p>
+            <p className='w-2/12 sm:w-1/12'>Rank</p>
+            <p className='w-8/12 sm:w-10/12'>Player</p>
+            <p className='w-2/12 sm:w-1/12'>Level</p>
           </div>
 
           {/* LEADERBOARD DATA */}
@@ -42,11 +38,11 @@ const UserLeaderboard = ({ users }) => {
               <div key={score.id} className={score.username === localStorage.getItem('username') ?
                 `flex text-center font-medium text-graybutton bg-white px-2 py-1 hover:opacity-60 hover:bg-white hover:text-graybutton` :
                 `flex text-center font-medium ${index % 2 ? 'bg-gray-600' : 'bg-gray-500'} px-2 py-1 hover:opacity-60`}>
-                <p className='w-1/12'>{index + 1}</p>
-                <p className='w-10/12'>
+                <p className='w-2/12 sm:w-1/12'>{index + 1}</p>
+                <p className='w-8/12 sm:w-10/12'>
                   {score.username}
                 </p>
-                <p className='w-1/12'>
+                <p className='w-2/12 sm:w-1/12'>
                   {levelUp(score.total_experience_points).level}
                 </p>
               </div>
